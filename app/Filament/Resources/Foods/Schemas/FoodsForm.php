04cbe86a75000b25e92a72d2c31c5a36f365e3ae
Syahrul Setiawan <syahrulsetiawan2801@gmail.com>
2025-12-04 02:16:18 +0700

    CMS Food resources

--- app/Filament/Resources/Foods/Schemas/FoodsForm.php
+++ app/Filament/Resources/Foods/Schemas/FoodsForm.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\Foods\Schemas;
 
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
@@ -13,22 +16,52 @@ class FoodsForm
         return $schema
             ->components([
                 TextInput::make('name')
-                    ->required(),
-                TextInput::make('description')
-                    ->required(),
+                    ->required()
+                    ->columnSpanFull(),
+                RichEditor::make('description')
+                    ->required()
+                    ->columnSpanFull(),
                 FileUpload::make('image')
                     ->image()
-                    ->required(),
+                    ->directory('foods')
+                    ->required()
+                    ->columnSpanFull(),
                 TextInput::make('price')
-                    ->required(),
+                    ->required()
+                    ->numeric()
+                    ->columnSpanFull()
+                    ->prefix('Rp')
+                    ->reactive(),
+                Toggle::make('is_promo')
+                    ->reactive(),
+                Select::make('percent')
+                    ->options([
+                        10 => '10%',
+                        25 => '25%',
+                        35 => '35%',
+                        50 => '50%',
+                    ])
+                    ->columnSpanFull()
+                    ->reactive()
+                    ->hidden(fn ($get) => !$get('is_promo'))
+                    ->afterStateUpdated(function ($set, $get, $state) {
+                        if ($get('is_promo') && $get('price') && $get('percent')) {
+                            $discount = ($get('price') * (int)$get('percent')) / 100;
+                            $set('price_afterdiscount', $get('price') - $discount);
+                        } else {
+                            $set('price_afterdiscount', $get('price'));
+                        }
+                    }),
                 TextInput::make('price_afterdiscount')
-                    ->default(null),
-                TextInput::make('percent')
-                    ->default(null),
-                TextInput::make('is_promo')
-                    ->default(null),
-                TextInput::make('categories_id')
-                    ->required(),
+                    ->numeric()
+                    ->prefix('Rp')
+                    ->readOnly()
+                    ->columnSpanFull()
+                    ->hidden(fn( $get) => !$get('is_promo')),
+                Select::make('categories_id')
+                    ->required()
+                    ->columnSpanFull()
+                    ->relationship('categories', 'name'),
             ]);
     }
 }
